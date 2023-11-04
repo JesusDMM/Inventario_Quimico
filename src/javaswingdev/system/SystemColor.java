52358412a90c0fd473a2b20af2750c0ecf8eecd7
JovanyCm1 <jaime.clasess@gmail.com>
2023-11-04 15:44:52 -0600

    Se agregaron un chorronal de cosas

---
+++ src/javaswingdev/system/SystemColor.java
@@ -0,0 +1,9 @@
+package javaswingdev.system;
+
+import java.awt.Color;
+
+public class SystemColor {
+
+    public final static Color MAIN_COLOR_1 = Color.decode("#ED213A");
+    public final static Color MAIN_COLOR_2 = Color.decode("#93291E");
+}
